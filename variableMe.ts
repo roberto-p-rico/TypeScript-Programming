0b8662e154744440cf1435af435a7dae6375d881
Roberto Rico <104702856+robertoricop@users.noreply.github.com>
2023-09-08 12:28:13 -0400

    Update variableMe.ts

--- variableMe.ts
+++ variableMe.ts
@@ -1,10 +1,27 @@
 let greetings: string = "Hello Roberto";
 
-let mynum = 6
-
 greetings.toLowerCase()
-
 console.log(greetings);
 
+// number
+
+let userId: number = 334455.3
+
+userId.toFixed()
+// userId = "Roberto"
+
+// boolean
+
+let isLoggedIn: boolean = false
+
+// any
+
+let hero;
+
+function getHero(){
+    return "Ironman"
+}
+
+hero = getHero()
 
 export {}

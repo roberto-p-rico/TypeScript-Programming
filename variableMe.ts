ec17ca4e2c9fb74499ca3d5c5c3958b6eb2a5e45
Roberto Rico <104702856+robertoricop@users.noreply.github.com>
2023-09-08 12:45:42 -0400

    Create variableMe.ts

---
+++ variableMe.ts
@@ -0,0 +1,27 @@
+let greetings: string = "Hello Roberto";
+
+greetings.toLowerCase()
+console.log(greetings);
+
+// number
+
+let userId: number = 334455.3
+
+userId.toFixed()
+// userId = "Roberto"
+
+// boolean
+
+let isLoggedIn: boolean = false
+
+// any
+
+let hero;
+
+function getHero(){
+    return "Ironman"
+}
+
+hero = getHero()
+
+export {}

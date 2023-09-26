92f1b427ab2cc0df456d2a000c9049357b1cde20
Roberto Rico <104702856+robertoricop@users.noreply.github.com>
2023-09-26 12:43:38 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,12 +1,21 @@
+//  class User {
+    //  public email: string
+    //  private name: string
+    //  readonly city: string = "Venice"
+    //  constructor(email: string, name: string){
+        //  this.email = email;
+        //  this.name = name
+    //  }
+// }
+
 class User {
-    email: string
-    name: string
-    readonly city: string = ""
-    constructor(email: string, name: string){
-        this.email = email;
+    constructor(email, name) {
+        this.email = email
         this.name = name
+        this.userId = userId;
+        this.city = "Venice";
     }
 }
-
-const Roberto = new User("R@g.com","Roberto")
-// Roberto.city = "Venice"
+  
+const Roberto = new User("R@g.com", "Roberto")
+// Roberto.name

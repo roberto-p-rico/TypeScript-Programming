e1728ab3dc13884b9ab6bb49381c5ba22ffbf4c2
Roberto Rico <104702856+robertoricop@users.noreply.github.com>
2023-09-26 12:22:01 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
-"user strict";
-console.log("typescript is here still");
+class User {
+    email: string
+    name: string
+    readonly city: string = ""
+    constructor(email: string, name: string){
+        this.email = email;
+        this.name = name
+    }
+}
+
+const Roberto = new User("R@g.com","Roberto")
+// Roberto.city = "Venice"

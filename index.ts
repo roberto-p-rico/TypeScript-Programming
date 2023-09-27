9b5ba7668c3033a74ec6232cfaecffa784516b8f
Roberto Rico <104702856+robertoricop@users.noreply.github.com>
2023-09-27 12:46:34 -0400

    Update index.ts

--- index.ts
+++ index.ts
@@ -9,14 +9,45 @@
 // }
 
 class User {
-    readonly city: string = "Venice"
+    protected _courseCount = 1;
+
+    readonly city: string = "Venice";
+
     constructor(
         public email: string, 
         public name: string
-        // private userId: string
-        ){
+    ) {
+    }
+
+    private deleteToken() {
+        console.log("Token deleted");
+    }
+
+    get getAppleEmail(): string {
+        return `apple${this.email}`;
+    }
+
+    get courseCount(): number {
+        return this._courseCount;
+    }
+
+    set courseCount(courseNum: number) {
+        if (courseNum <= 1) {
+            throw new Error("Course count should be more than 1");
+        }
+        this._courseCount = courseNum;
     }
 }
 
-const Roberto = new User("R@g.com","Roberto")
-// Roberto.name
+class SubUser extends User {
+    isFamily: boolean = true
+    changeCourseCount(){
+        this._courseCount = 4
+    }
+}
+
+const Roberto = new User("R@g.com", "Roberto");
+
+// console.log(Roberto.name);
+
+// Roberto.deleteToken();

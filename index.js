601f560d95c372e4194ef9584a02acbc0c21c77f
Roberto Rico <104702856+robertoricop@users.noreply.github.com>
2023-09-27 12:47:13 -0400

    Update index.js

--- index.js
+++ index.js
@@ -10,12 +10,45 @@
 
 class User {
     constructor(email, name) {
-        this.email = email
-        this.name = name
-        this.userId = userId;
+        this.email = email;
+        this.name = name;
+        this._courseCount = 1;
         this.city = "Venice";
     }
+
+    deleteToken() {
+        console.log("Token deleted");
+    }
+
+    get getAppleEmail() {
+        return `apple${this.email}`;
+    }
+
+    get courseCount() {
+        return this._courseCount;
+    }
+
+    set courseCount(courseNum) {
+        if (courseNum <= 1) {
+            throw new Error("Course count should be more than 1");
+        }
+        this._courseCount = courseNum;
+    }
 }
-  
-const Roberto = new User("R@g.com", "Roberto")
-// Roberto.name
+
+class SubUser extends User {
+    constructor() {
+        super(...arguments);
+        this.isFamily = true;
+    }
+
+    changeCourseCount() {
+        this._courseCount = 4;
+    }
+}
+
+const Roberto = new User("R@g.com", "Roberto");
+
+// console.log(Roberto.name);
+
+// Roberto.deleteToken();

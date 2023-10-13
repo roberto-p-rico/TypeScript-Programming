4291f6faed1663b66b21885186dd312f48e6fb00
Roberto Rico <104702856+robertoricop@users.noreply.github.com>
2023-10-12 20:12:58 -0400

    Create detection.js

---
+++ detection.js
@@ -0,0 +1,26 @@
+function detectType(val) {
+    if (typeof val === "string") {
+        return val.toLowerCase();
+    }
+    return val + 3;
+}
+
+function provideId(id) {
+    if (!id) {
+        console.log("Please provide ID");
+        return;
+    }
+    id.toLowerCase();
+}
+
+function printAll(strs) {
+    if (strs) {
+        if (typeof strs === "object") {
+            for (const s of strs) {
+                console.log(s);
+            }
+        } else if (typeof strs === "string") {
+            console.log(strs);
+        }
+    }
+}
